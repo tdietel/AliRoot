70f2d1f43119624c63ee345f915842e6b7439d95
Tom Dietel <tom@dietel.net>
2018-10-14 00:56:19 +0200

    Implement tools to fix mixed TRAPconfigs
    This is a re-implementation of Hans Beck's "Plurality" branch without
    STL, to be used to overwrite mixed TRAP configurations in the OCDB with
    the most frequent configuration. This commit, once merged into AliRoot
    master, will make it possible to patch the OCDB with plain AliRoot.

--- TRD/TRDbase/AliTRDCalDCSv2.cxx
+++ TRD/TRDbase/AliTRDCalDCSv2.cxx
@@ -17,7 +17,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                           //
-//  TRD calibration class v2 for TRD DCS parameters                             //
+//  TRD calibration class v2 for TRD DCS parameters                          //
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -26,7 +26,9 @@
 #include "AliTRDCalDCSGTU.h"
 #include "AliLog.h"
 
+#include <iostream>
 
+#include <TH1.h>
 
 ClassImp(AliTRDCalDCSv2)
   
@@ -148,6 +150,8 @@ void AliTRDCalDCSv2::Print(Option_t *option) const
 	       fStartTime, fEndTime, fEndTime-fStartTime));
   
 
+  // ------------------------------------------------------------------------
+  // display FEE configuration for all ROCs
   for(Int_t i=0; i<540; i++) {
     AliTRDCalDCSFEEv2 *iDCSFEEObj = GetCalDCSFEEObj(i);
     if(iDCSFEEObj == NULL) continue;
@@ -163,9 +167,140 @@ void AliTRDCalDCSv2::Print(Option_t *option) const
 		 iDCSFEEObj->GetConfigVersion().Data()
 		 ));
   }
+
+  // ------------------------------------------------------------------------
+  // display summary of FEE configurations
+
+  TH1F* h = CreatePluralityHistogram(&AliTRDCalDCSFEEv2::GetConfigNameVersion);
+  for (Int_t i=1; i<=h->GetNbinsX(); i++) {
+    AliInfoStream() << std::setw(4) << h->GetBinContent(i) << "   "
+		    << h->GetXaxis()->GetBinLabel(i) << "\n";
+  }
+  delete h;
+  
 }
 
+//_____________________________________________________________________________
+void AliTRDCalDCSv2::PrintGlobalConfig(Option_t *option) const
+{
+  AliInfoStream()
+    << "fGNumberOfTimeBins    - " << fGNumberOfTimeBins << "\n"
+    << "fGConfigTag           - " << fGConfigTag << "\n"
+    << "fGSingleHitThres      - " << fGSingleHitThres << "\n"
+    << "fGThreePadClustThres  - " << fGThreePadClustThres << "\n"
+    << "fGSelNoZS             - " << fGSelNoZS << "\n"
+    << "fGTCFilterWeight      - " << fGTCFilterWeight << "\n"
+    << "fGTCFilterShortDecPar - " << fGTCFilterShortDecPar << "\n"
+    << "fGTCFilterLongDecPar  - " << fGTCFilterLongDecPar << "\n"
+    << "fGFastStatNoise       - " << fGFastStatNoise << "\n"
+    << "fGConfigVersion       - " << fGConfigVersion << "\n"
+    << "fGConfigName          - " << fGConfigName << "\n"
+    << "fGFilterType          - " << fGFilterType << "\n"
+    << "fGReadoutParam        - " << fGReadoutParam << "\n"
+    << "fGTestPattern         - " << fGTestPattern << "\n"
+    << "fGTrackletMode        - " << fGTrackletMode << "\n"
+    << "fGTrackletDef         - " << fGTrackletDef << "\n"
+    << "fGTriggerSetup        - " << fGTriggerSetup << "\n"
+    << "fGAddOptions          - " << fGAddOptions << "\n";
+}
 
+//____________________________________________________________________________
+TH1F* AliTRDCalDCSv2::CreatePluralityHistogram(TString (AliTRDCalDCSFEEv2::*fct)() const) const
+{
+  // Create a histogram with alphanumernic labels from a function in
+  // the AliTRDCalDCSFEEv2 configuration class. This function can be
+  // used to find the frequency of configuration values in
+  // AliTRDCalDCSFEEv2.
+
+  // Code based on an example on alphanumeric labels in the ROOT docs, see:
+  // https://root.cern.ch/doc/master/hlabels1_8C.html
+
+  // create the histogram and enable automatic extension 
+  TH1F *h = new TH1F("h","test",3,0.0,3.0);
+  h->SetStats(0); // disable statistics display
+  h->SetCanExtend(TH1::kAllAxes); // 
+  
+  // loop over all 540 chambers
+  for(Int_t i=0; i<540; i++) {
+    AliTRDCalDCSFEEv2 *iDCSFEEObj = GetCalDCSFEEObj(i);
+    if(iDCSFEEObj == NULL) {
+      h->Fill("+++ NOT PRESENT +++",1);
+      continue;
+    }
+    
+    if(iDCSFEEObj->GetStatusBit() != 0) {
+      h->Fill("+++ STATUS BIT SET +++",1);
+      continue;
+    }
+
+    h->Fill( (iDCSFEEObj->*fct)(),1);
+  }
+
+  // compress and reorder bins
+  h->LabelsDeflate();
+  h->LabelsOption(">");
+
+  return h;
+  
+}
+
+//____________________________________________________________________________
+AliTRDCalDCSFEEv2* AliTRDCalDCSv2::FindPluralityChamber(TString (AliTRDCalDCSFEEv2::*fct)() const) const
+{
+
+  // find the plurality name
+  TH1F* h = CreatePluralityHistogram(fct);
+  TString plurality = h->GetXaxis()->GetBinLabel(1);
+  delete h;
+
+  // loop over all chambers to find one with plurality name
+  for(Int_t i=0; i<540; i++) {
+    AliTRDCalDCSFEEv2 *iDCSFEEObj = GetCalDCSFEEObj(i);
+    if(iDCSFEEObj == NULL) {
+      h->Fill("+++ NOT PRESENT +++",1);
+      continue;
+    }
+    
+    if(iDCSFEEObj->GetStatusBit() != 0) {
+      h->Fill("+++ STATUS BIT SET +++",1);
+      continue;
+    }
+
+    if ( (iDCSFEEObj->*fct)() == plurality ) {
+      return iDCSFEEObj;
+    }
+  }
+
+  // we should never get here
+  AliFatal("No chamber with plurality configuration found!");
+  return NULL;
+}
+
+//____________________________________________________________________________
+void AliTRDCalDCSv2::ForceOverwritePluralityConfig()
+{
+  AliTRDCalDCSFEEv2* plcfg;
+  plcfg = FindPluralityChamber(&AliTRDCalDCSFEEv2::GetConfigNameVersion);
+
+  fGNumberOfTimeBins    = plcfg->GetNumberOfTimeBins();
+  fGConfigTag           = plcfg->GetConfigTag();
+  fGSingleHitThres      = plcfg->GetSingleHitThres();
+  fGThreePadClustThres  = plcfg->GetThreePadClustThres();
+  fGSelNoZS             = plcfg->GetSelectiveNoZS();
+  fGTCFilterWeight      = plcfg->GetTCFilterWeight();
+  fGTCFilterShortDecPar = plcfg->GetTCFilterShortDecPar();
+  fGTCFilterLongDecPar  = plcfg->GetTCFilterLongDecPar();
+  fGFastStatNoise       = plcfg->GetFastStatNoise();
+  fGConfigVersion       = plcfg->GetConfigVersion();
+  fGConfigName          = plcfg->GetConfigName();
+  fGFilterType          = plcfg->GetFilterType();
+  fGReadoutParam        = plcfg->GetReadoutParam();
+  fGTestPattern         = plcfg->GetTestPattern();
+  fGTrackletMode        = plcfg->GetTrackletMode();
+  fGTrackletDef         = plcfg->GetTrackletDef();
+  fGTriggerSetup        = plcfg->GetTriggerSetup();
+  fGAddOptions          = plcfg->GetAddOptions();
+}
 
 
 //_____________________________________________________________________________
